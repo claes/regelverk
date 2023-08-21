ccd15ee538fb84b1062ef12f197380206e007cf4
claes <claes@holmerson.net>
2023-08-21 20:36:27 +0200

    Add another publish

--- loop-test.go
+++ loop-test.go
@@ -66,13 +66,13 @@ func (l *testLoop) turnOnAmpWhenTVOn(ev MQTTEvent) []MQTTPublish {
 						Retained: false,
 						Wait:     3 * time.Second,
 					},
-					MQTTPublish{
+					{
 						Topic:    "samsungremote/key/reconnectsend",
 						Payload:  "KEY_VOLDOWN",
 						Qos:      2,
 						Retained: false,
 						Wait:     10 * time.Second,
-					}
+					},
 				}
 				// Need to wait here since a newly started TV is not receptive first 20 or so seconds
 				for i := int64(25); i < 20; i++ {
