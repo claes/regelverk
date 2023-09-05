aaac799e626e5bf1d269173a55ea911252edc595
claes <claes@holmerson.net>
2023-09-05 21:49:44 +0200

    Websockets to show updated state

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require github.com/eclipse/paho.mqtt.golang v1.4.3
 
 require (
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
--- http-loop-rotel.go
+++ http-loop-rotel.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"sort"
+
+	"github.com/gorilla/websocket"
 )
 
 //go:embed templates/rotel.html
 var content2 embed.FS
+var rotelStateUpdated = make(chan struct{})
 
 type rotelHttpLoop struct {
 	statusLoop
@@ -19,13 +23,15 @@ func (l *rotelHttpLoop) Init() {
 	fmt.Println("Rotel http init")
 	http.HandleFunc("/", l.mainHandler)
 	http.HandleFunc("/rotel/state", l.stateHandler)
+	http.HandleFunc("/rotel/state/init", l.rotelStateInitWs)
+	http.HandleFunc("/rotel/state/ws", l.rotelStateWs)
 }
 
 func (l *rotelHttpLoop) ProcessEvent(ev MQTTEvent) []MQTTPublish {
 	switch ev.Topic {
 	case "rotel/state":
 		l.rotelState = parseJSONPayload(ev)
-		// l.volume = m["volume"].(string)
+		rotelStateUpdated <- struct{}{}
 	}
 
 	return nil
@@ -55,9 +61,68 @@ func (l *rotelHttpLoop) mainHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (l *rotelHttpLoop) stateHandler(w http.ResponseWriter, r *http.Request) {
+
+	var keys []string
+	for k := range l.rotelState {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
 	fmt.Fprintf(w, "<div>")
-	for key, value := range l.rotelState {
-		fmt.Fprintf(w, "<div>%s: %s</div>", key, value)
+	for _, key := range keys {
+		fmt.Fprintf(w, "<div>%s: %s</div>", key, l.rotelState[key])
 	}
 	fmt.Fprintf(w, "</div>")
+}
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func (l *rotelHttpLoop) rotelStateInitWs(w http.ResponseWriter, req *http.Request) {
+	var responseTemplate = `
+	<div id="ws-output" hx-ext="ws" ws-connect="/rotel/state/ws">	
+		<div id="rotel-state"></div>
+	</div>
+	`
+
+	tmpl := template.New("ws-output")
+	tmpl.Parse(responseTemplate)
+
+	tmpl.Execute(w, nil)
+}
+
+func (l *rotelHttpLoop) rotelStateWs(w http.ResponseWriter, req *http.Request) {
+
+	c, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		fmt.Printf("Error trying to upgrade: %v\n", err)
+		return
+	}
+	defer c.Close()
+
+	for {
+		socketWriter, err := c.NextWriter(websocket.TextMessage)
+
+		if err != nil {
+			fmt.Printf("Error getting socket writer %v\n", err)
+			break
+		}
+
+		var keys []string
+		for k := range l.rotelState {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		fmt.Fprintf(socketWriter, "<div id='rotel-state' hx-swap-oob='true'>")
+		for _, key := range keys {
+			fmt.Fprintf(socketWriter, "<div>%s: %s</div>", key, l.rotelState[key])
+		}
+		fmt.Fprintf(socketWriter, "</div>")
+
+		socketWriter.Close()
+
+		select {
+		case <-rotelStateUpdated:
+			continue
+		}
+	}
 }
