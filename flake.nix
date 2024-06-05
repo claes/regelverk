0b0da9474ea1a8c6ff280b25bda701367826e804
claes <claes@holmerson.net>
2024-06-05 19:21:06 +0200

    Depdency update

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
 
         buildInputs = [pkgs.libcec pkgs.libcec_platform];
 
-        vendorHash = "sha256-oZl1lftwpfS8H9y2u2MAu2zrFqXmwiGk4YMaXH/xzno=";
+        vendorHash = "sha256-uumpPPUU4rRgT8i8Gne/HYK4V4MZXEQu4kjJWd0/9ZE=";
       };
     });
 
