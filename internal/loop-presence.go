16944bfe3e37b57c677689849a7330713315e89c
claes <claes@holmerson.net>
2024-11-21 20:08:48 +0100

    Begin work with wificlients

--- internal/loop-presence.go
+++ internal/loop-presence.go
@@ -1,8 +1,12 @@
 package regelverk
 
 import (
+	"encoding/json"
 	"fmt"
+	"log/slog"
 	"time"
+
+	routerosmqtt "github.com/claes/routeros-mqtt/lib"
 )
 
 type PresenceLoop struct {
@@ -10,12 +14,41 @@ type PresenceLoop struct {
 	livingroomLastAbsence  time.Time
 	livingroomLastPresence time.Time
 	livingroomPresence     bool
+	phoneWifiLastPresence  time.Time
+	phoneWifiPresence      bool
 }
 
 func (l *PresenceLoop) Init(m *mqttMessageHandler, config Config) {}
 
 func (l *PresenceLoop) processPresence(ev MQTTEvent) []MQTTPublish {
 	switch ev.Topic {
+
+	case "routeros/wificlients":
+		var wifiClients []routerosmqtt.WifiClient
+
+		err := json.Unmarshal(ev.Payload.([]byte), &wifiClients)
+		if err != nil {
+			slog.Debug("Could not parse payload", "topic", "routeros/wificlients")
+		}
+		found := false
+		for _, wifiClient := range wifiClients {
+			if wifiClient.MacAddress == "AA:73:49:2B:D8:45" {
+				found = true
+				l.phoneWifiLastPresence = time.Now()
+				break
+			}
+		}
+		l.phoneWifiPresence = found
+
+		return []MQTTPublish{
+			{
+				Topic: "regelverk/presence/phone",
+				Payload: fmt.Sprintf("{\"present\": \"%t\"}",
+					l.phoneWifiPresence),
+				Qos:      2,
+				Retained: true,
+			},
+		}
 
 	case "zigbee2mqtt/livingroom-presence":
 		m := parseJSONPayload(ev)
