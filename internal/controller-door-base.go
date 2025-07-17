f7151dfc00f3afb7727d52937603ec94fde8eab6
claes <claes@holmerson.net>
2025-07-17 21:50:44 +0200

    Remove debug code

--- internal/controller-door-base.go
+++ internal/controller-door-base.go
@@ -96,13 +96,6 @@ func (c *DoorReminderController) startNotifyDoorOpen(parentContext context.Conte
 						Retained: false,
 						Wait:     0 * time.Second,
 					},
-					{
-						Topic:    "telegram/regelverkgeneral/send",
-						Payload:  "test battery reminder",
-						Qos:      2,
-						Retained: false,
-						Wait:     0 * time.Second,
-					},
 				}
 				c.addEventsToPublish(events)
 				i = i + 1
