55ad2b4d1288c4afbe8dbf58528c2818d3559714
claes <claes@holmerson.net>
2025-07-17 21:38:10 +0200

    Include telegram bridge wrapper

--- cmd/regelverk-hub/main.go
+++ cmd/regelverk-hub/main.go
@@ -19,6 +19,7 @@ func main() {
 		&internal.RouterOSBridgeWrapper{},
 		&internal.SamsungBridgeWrapper{},
 		&internal.SnapcastBridgeWrapper{},
+		&internal.TelegramBridgeWrapper{},
 	}
 
 	controllers := &[]internal.Controller{
