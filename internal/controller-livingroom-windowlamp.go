c21c9abf95623a8cf4cf305ac74470e4092fa2db
claes <claes@holmerson.net>
2026-03-22 20:57:36 +0100

    Add log temporarily

--- internal/controller-livingroom-windowlamp.go
+++ internal/controller-livingroom-windowlamp.go
@@ -2,6 +2,7 @@ package regelverk
 
 import (
 	"context"
+	"log/slog"
 	"reflect"
 	"time"
 
@@ -61,6 +62,7 @@ func (c *LivingroomWindowlampController) Initialize(masterController *MasterCont
 func (c *LivingroomWindowlampController) createTriggers(ev MQTTEvent) []string {
 	phaseOfDay, found := processType[PhaseOfDay](ev, "regelverk/ticker/phaseofday")
 	if found {
+		slog.Info("Creating triggers for livingroom windowlamp", "phaseOfDay", phaseOfDay)
 		if phaseOfDay.Meridiem == PostMeridiem &&
 			(phaseOfDay.SolarPhase == Nighttime ||
 				phaseOfDay.SolarPhase == EveningAstronomcialTwilight ||
